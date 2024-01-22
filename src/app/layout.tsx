a6dc3bf83b03d654b7eeadf37fe6ef5ed613b048
Tomas Garcia <tomas.garcia@zolar.de>
2024-01-22 09:20:15 +0100

    Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 import './globals.css'
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 const poppins = Poppins({ subsets: ['latin'], weight: ["400", "500", "700", "900"], style: ["italic", "normal"], variable: "--font-poppins" })
 
@@ -17,11 +19,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={poppins.className}>
-        <main className='font-normal'>
-          {/* ?Header */}
-          {children}
-          {/* ?Footer */}
-        </main>
+          <main className='font-normal'>
+            <Header />
+            {children}
+            <Footer />
+          </main>
         </body>
     </html>
   )
