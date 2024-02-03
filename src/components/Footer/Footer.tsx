0a5165504b5e561859690c7119e80eb01b908961
Tomas Garcia <tomas.garcia@zolar.de>
2024-02-03 22:00:35 +0100

    Authentication Page Setup

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -17,15 +17,15 @@ const Footer = () => {
             <p>123 Road</p>
             <div className='flex items-center py-4'>
               <BsFillSendFill />
-              <p className='ml-2'>codewithlari</p>
+              <p className='ml-2'>TomasGarciaDev</p>
             </div>
             <div className='flex items-center'>
               <BsTelephoneOutbound />
-              <p className='ml-2'>000-000-00</p>
+              <p className='ml-2'>+43 669525179</p>
             </div>
             <div className='flex items-center pt-4'>
               <BiMessageDetail />
-              <p className='ml-2'>codewithlari</p>
+              <p className='ml-2'>tomasgarciadev@gmail.com</p>
             </div>
           </div>
 
